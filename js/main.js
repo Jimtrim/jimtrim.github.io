4f382c1e0b4816f34da1a8ae0676e2554b1647fb
jimtrim <jimfrodehoff@gmail.com>
2014-06-22 20:50:03 +0200

    js cleanup

--- js/main.js
+++ js/main.js
@@ -1,25 +1,35 @@
 jQuery.fn.pages = function(control) {
+
+	// plugin to add fading page navigation
+	// usage: jQuery("<LIST_OF_LI_ELEMENTS>").pages('<ELEMENT_CONTAINING_CONTENT>');
+
+	var FADE_SPEED = 400; 
+
 	var element = jQuery(this);
 	control = jQuery(control);
 
 	element.bind("change.page", function(e, pageName) { // change active link and hash
 		element.find("li").removeClass("active");
 		element.find(">[data-page='" + pageName + "']").addClass('active');
-		console.log("recievedEvent: change.page");
 	});
 
 	element.bind("change.page", function(e, pageName) { // change content
-		control.fadeOut('400', function() {
+		control.fadeOut(FADE_SPEED, function() {
 			control.find(">[data-page]").removeClass("active");
 			control.find(">[data-page='" + pageName + "']").addClass('active');
-			control.fadeIn('400');
+			control.fadeIn(FADE_SPEED);
 		});
 	});
+
 
 	if (window.location.hash == "") {
 		window.location.hash = element.find('li:first').attr("data-page");
 	}
-	console.log("intial trigger");
+	jQuery(window).bind('hashchange', function(event) {
+		var pageName = window.location.hash.slice(1);
+		jQuery("#main-navigation").trigger('change.page', pageName);
+	});
+
 	element.trigger("change.page", window.location.hash.substring(1));
 
 	return this;
@@ -29,11 +39,6 @@ jQuery(document).ready(function($) {
 	
 	jQuery("#main-navigation").pages('#main-content'); // connect navigation and content divs
 	
-	jQuery(window).bind('hashchange', function(event) {
-		var pageName = window.location.hash.slice(1);
-		jQuery("#main-navigation").trigger('change.page', pageName);
-		console.log("hashchange:"+pageName);
-		
-	});
+	
 	
 });
